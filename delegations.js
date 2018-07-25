33b92a84fe8f54b1bbfa0a430327c0b670cf2413
Cryptouru <cryptouru@gmail.com>
2018-07-24 23:04:51 -0300

    process delegations

--- delegations.js
+++ delegations.js
@@ -1,5 +1,7 @@
 const dsteem = require('dsteem')
 const client = new dsteem.Client('https://api.steemit.com')
+const _ = require('lodash')
+const moment = require('moment')
 const utils = require('./utils')
 const mail = require('./mail')
 
@@ -25,7 +27,8 @@ MongoClient.connect(config.mongo_uri, async function (err, dbClient) {
     db = dbClient.db(dbName)
     // Get the documents collection
     collection = db.collection(collectionName)
-    startProcess()
+    // startProcess()
+    processRewards()
   } else {
     utils.log(err, 'delegations')
     mail.sendPlainMail('Database Error', err, config.report_emails)
@@ -94,6 +97,7 @@ async function getDelegations (account, start, end) {
     // console.log(transactions)
   } catch (err) {
     console.log(err)
+    // Consider exponential backoff if extreme cases start happening
     if (err.type === 'request-timeout' || err.type === 'body-timeout') getDelegations(account, start, end)
   }
 }
@@ -118,4 +122,69 @@ async function updateActiveDelegations () {
   let activeDelegations = await query.toArray()
   await db.collection('active_delegations').drop()
   return db.collection('active_delegations').insert(activeDelegations)
+}
+
+async function processRewards () {
+  let rewards = []
+  let date = new Date()
+  let note = 'Delegation Reward Until EOD ' + date
+  // Get delegators who did not change their amount on the week
+  let fullWeekers = await db.collection('active_delegations').find(
+    {'tx_date': {$lte: new Date('2018-07-09')}}).toArray()
+  rewards = _.map(fullWeekers, function (o) {
+    return {
+      user: o._id,
+      token_count: +(o.steem_power * 7).toFixed(3),
+      reward_activity: 'Delegation',
+      note: note
+    }
+  })
+
+  // console.log(fullWeekers)
+  // Get transactions of the processed week
+  let weekTxs = await db.collection('delegation_transactions').find(
+    {'tx_date': {$gt: new Date('2018-07-09'), $lte: new Date('2018-07-16')}}).sort({tx_date: 1}).toArray()
+  
+  // console.log(weekTxs)
+  let groupedTxs = _.groupBy(weekTxs, 'delegator')
+  // console.log(groupedTxs)
+  for (let index in groupedTxs) {
+    if (groupedTxs[index].length > 1) {
+      let totalPower = 0
+      let user = index
+      console.log('Multi week txs')
+      for (let i = 0; i < groupedTxs[index].length; i++) {
+        let txs = groupedTxs[index][i]
+        console.log(txs)
+
+        let start = moment(txs.tx_date)
+        let end
+        if (i !== groupedTxs[index].length - 1) end = moment(groupedTxs[index][i + 1].tx_date)
+        else end = moment('2018-07-16')
+        let activeHours = end.diff(start, 'hours')
+        console.log(activeHours)
+
+        let newPower = activeHours * (txs.steem_power / 24)
+        totalPower = totalPower + newPower
+        console.log(totalPower)
+      }
+      totalPower = +totalPower.toFixed(3)
+      rewards.push({user: user, token_count: totalPower, reward_activity: 'Delegation', date: date, note: note})
+    } else {
+      let txs = groupedTxs[index][0]
+      console.log('Just one weekly txs')
+      console.log(txs)
+      let start = moment(txs.tx_date)
+      let end = moment('2018-07-16')
+      let activeHours = end.diff(start, 'hours')
+      console.log(activeHours)
+      let totalPower = activeHours * (txs.steem_power / 24)
+      totalPower = +totalPower.toFixed(3)
+      console.log(totalPower)
+      rewards.push({user: txs.delegator, token_count: totalPower, reward_activity: 'Delegation', date: date, note: note})
+    }
+  }
+  console.log('--- REWARDS ---')
+  rewards = _.orderBy(rewards, ['user', 'token_count'], ['asc', 'desc'])
+  console.log(rewards)
 }
