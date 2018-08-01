ecec97c0e62f498f6c1991d1296e9c0ab9accd1a
Greateck <dev.wp@greateck.com>
2018-08-01 22:54:28 +0300

    adding banned user @isufbakaj

--- utils.js
+++ utils.js
@@ -209,6 +209,13 @@ function format(n, c, d, t) {
     
     if(!benefit)
       continue;
+	  
+	  for (var n = 0; n < config.banned_users.length; n++) {
+			if (post.author === config.banned_users[n]){
+				utils.log('User '+post.author+' is banned, skipping his post:' + post.url);
+				continue;
+			}
+		  }   
 
     results.push(post);
   }
