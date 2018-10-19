e08c98f7a9c4579c16e95330301b4ca776f8670e
Greateck <dev.wp@greateck.com>
2018-10-19 10:53:24 +0300

    fix for incorrect step count var

--- curation-bot.js
+++ curation-bot.js
@@ -896,7 +896,7 @@ function sendComment(post, vote_weight) {
 			//replace(/\{milestone\}/g, milestone_txt).
 			
 			//adding proper meta content for later relevant reward via afit_tokens data
-			var jsonMetadata = { tags: ['actifit'], app: 'actifit/v'+version, activity_count: post.step_count, user_rank: post.user_rank, content_score: post.content_score, media_score: post.media_score, upvote_score: post.upvote_score, comment_score: post.comment_score, user_rank_score: post.user_rank_score, moderator_score: post.moderator_score, post_activity_score: post.activity_score, afit_tokens: token_count, post_upvote: vote_weight };
+			var jsonMetadata = { tags: ['actifit'], app: 'actifit/v'+version, activity_count: post_step_count, user_rank: post.user_rank, content_score: post.content_score, media_score: post.media_score, upvote_score: post.upvote_score, comment_score: post.comment_score, user_rank_score: post.user_rank_score, moderator_score: post.moderator_score, post_activity_score: post.activity_score, afit_tokens: token_count, post_upvote: vote_weight };
 			if (!config.testing){
 				// Broadcast the comment
 				steem.broadcast.comment(config.posting_key, parentAuthor, parentPermlink, account.name, permlink, permlink, content, jsonMetadata, function (err, result) {
