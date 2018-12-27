30243ff61ac639dbcb5cb5987ae54884d0261bc7
Greateck <dev.wp@greateck.com>
2018-12-27 12:58:06 +0200

    adding actifit.io to eenabled hosts

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ MongoClient.connect(url, function(err, client) {
 
 //allows setting acceptable origins to be included across all function calls
 app.use(function(req, res, next) {
-  var allowedOrigins = ['*'];
+  var allowedOrigins = ['*', 'actifit.io'];
   var origin = req.headers.origin;
   if(allowedOrigins.indexOf(origin) > -1){
 	   res.setHeader('Access-Control-Allow-Origin', origin);
