4971239044c44047f88c8589117d2cad8f231f9d
Greateck <dev.wp@greateck.com>
2019-07-19 14:39:04 +0300

    fix url

--- app.js
+++ app.js
@@ -1558,7 +1558,8 @@ app.get("/downEbook", async function(req, res) {
 	  console.log(err);
 	  console.log('ebook does not exist');
 	  res.writeHead(400, {"Content-Type": "text/plain"});
-      res.end("ERROR File does not exist");
+      //res.end("ERROR File does not exist");
+	  res.end(err);
 	});
  
 })
